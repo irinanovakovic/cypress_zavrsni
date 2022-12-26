95cc1598f1bb656351c9b265ceb22a0011aadaeb
irinanovakovic <irina.karanovic@gmail.com>
2022-12-26 19:46:12 +0100

    added board

--- cypress/e2e/board.cy.js
+++ cypress/e2e/board.cy.js
@@ -1,10 +1,13 @@
+import{locators} from "../locators"
 describe('board page',() => {
     before(() => {
         cy.visit('/login')
         cy.login('irina.karanovic@gmail.com','BakicaVukica')
         cy.wait(4000)
-        it('Add board',()=> {
-            cy.visit('/my-organizations')
-        })
+       
+    })
+    it('Add board',()=> {
+        cy.visit('/my-organizations')
+        cy.get(locators.board.addNewBoard).click()
     })
 })
--- cypress/locators.js
+++ cypress/locators.js
@@ -3,5 +3,10 @@ export const locators = {
         email:'[name="email"]',
         password:'[type="password"]',
         loginBtn:'.vs-u-text--left > .vs-c-btn',
+     },
+     board: {
+        addNewBoard:'[title="Add new Board"]';
+        
      }
 }
+    
